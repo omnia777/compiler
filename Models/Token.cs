d3d7a61b1cf36ffb6e0da90803db1132008ed271
RanaWatany308 <102302089+RanaWatany308@users.noreply.github.com>
2022-05-21 17:46:59 +0200

    Add files via upload

---
+++ Models/Token.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Token
+    {
+        public string Type { get; set; }
+        public string Text { get; set; }
+
+        public int LineNum { get; set; }
+        public Token()
+        {
+
+        }
+        public Token(string type, string text)
+        {
+            this.Type = type;
+            this.Text = text;
+            this.LineNum = 1;
+
+        }
+    }
+    }
